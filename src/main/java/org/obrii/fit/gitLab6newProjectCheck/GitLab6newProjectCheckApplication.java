a0552fef7c4c761fd18ddc5c9c2f4608eeab4c02
Roman Mykolaichuk <mykroman@ukr.net>
2021-04-08 10:47:00 +0300

    1

---
+++ src/main/java/org/obrii/fit/gitLab6newProjectCheck/GitLab6newProjectCheckApplication.java
@@ -0,0 +1,13 @@
+package org.obrii.fit.gitLab6newProjectCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLab6newProjectCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLab6newProjectCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/obrii/fit/gitLab6newProjectCheck/GitLab6newProjectCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.obrii.fit.gitLab6newProjectCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLab6newProjectCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
